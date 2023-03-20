c46772108f4f495bcd7dc9d73e9d860a22c3ed0b
Benjaimico <benja124@live.cl>
2023-03-20 12:32:27 -0300

    cambios finales

---
+++ src/ListaUsuarios.java
@@ -0,0 +1,41 @@
+package sprintCapacitacion;
+
+import java.util.ArrayList;
+/**
+ * @Definicion: Clase que agrega un elemento al listado y llama al metodo analizarUsuario
+ * @Version: 1.0
+ * @Autor: Grupo Java (Jose Zamora, Benjamin, Fernanda, Javiera)
+ *
+ */
+public class ListaUsuarios {
+    ArrayList<Usuario> listaUsuarios;
+
+    public ArrayList<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public ListaUsuarios() { // Constructor de la clase listado que instancia un arreglo de usuarios
+        this.listaUsuarios =new ArrayList<Usuario>();
+    }
+
+    public void agregar(Usuario usuarios){ // Metodo que permite agregar Usuarios al listado
+        listaUsuarios.add(usuarios);
+    }
+
+
+    public void mostrarUsuarios() { // metodo que permite mostrar los diferentes tipos de Usuarios añadidos
+        for (int i = 0; i < listaUsuarios.size(); i++) {
+
+           // System.out.println(listaUsuarios.get(i).analizarUsuario());
+            System.out.println(listaUsuarios.get(i).getNombreUsuario() + listaUsuarios.get(i).getFechaNacimientoUsuario()+ listaUsuarios.get(i).getRunUsuario());
+
+        }
+    }
+
+}
+
+
